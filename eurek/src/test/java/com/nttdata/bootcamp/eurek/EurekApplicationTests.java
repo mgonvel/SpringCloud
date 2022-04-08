95afeb8bac546d0441b5306fad913eb9ce3999cf
Manuel González Vela <74413901+mgonvel@users.noreply.github.com>
2022-04-08 11:28:24 +0200

    Add files via upload

---
+++ eurek/src/test/java/com/nttdata/bootcamp/eurek/EurekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.eurek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
