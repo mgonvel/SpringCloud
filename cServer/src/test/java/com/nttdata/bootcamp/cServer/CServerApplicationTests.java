62bd9e354fb3ebb71c0728ed134a2ed3eaea9443
Manuel González Vela <74413901+mgonvel@users.noreply.github.com>
2022-04-08 11:24:46 +0200

    Add files via upload

---
+++ cServer/src/test/java/com/nttdata/bootcamp/cServer/CServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.cServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
